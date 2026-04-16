44d47ae8e77000d23a2b2be5354f3f7705b91b27
Lightning McQueen <bogdan.kaltrachian@gmail.com>
2026-04-16 11:21:01 +0200

    test de rembg

--- example_archi/backend/templates/remove_bg.py
+++ example_archi/backend/templates/remove_bg.py
@@ -2,8 +2,9 @@ from rembg import remove
 from PIL import Image
 
 input_path = "car_to_right.png"
-output_path = "cat_to_right_without_bg.png"
+output_path = "car_to_right_without_bg.png"
 
-with Image.open(input_path) as img:
-    result = remove(img)
-    result.save(output_path)
+with open(input_path, "rb") as i:
+    with open(output_path, "wb") as o:
+        o.write(remove(i.read()))
+
